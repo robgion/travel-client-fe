8f37c1922b0725b20bf30a7fb06708a32ca4d0e7
Alec <alec.mestroni@finconsgroup.com>
2022-05-04 16:27:23 +0200

    string correction

--- src/app/features/signup/components/signup-container/signup-container.component.ts
+++ src/app/features/signup/components/signup-container/signup-container.component.ts
@@ -8,8 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupContainerComponent implements OnInit {
 
-  currentEmail:String='';
-  currentPassword:String='';
+  currentEmail:string='';
+  currentPassword:string='';
   constructor() {
    }
   
